28d823e18e629bd437755952c5f28d2f26b48e72
Kenta Sato <bicycle1885@gmail.com>
2020-02-27 18:14:00 +0900

    add bam_get_mempolicy and bam_set_mempolicy

--- src/htslib/functions.jl
+++ src/htslib/functions.jl
@@ -1,8 +1,9 @@
 # Functions
 # =========
 #
-# Function definitions that are derived from C macros.  These are considered as
-# part of htslib's public API but not exported from the dynamic library.
+# Function definitions that are derived from C macros or static inline
+# functions.  These are considered as part of htslib's public API but not
+# exported from the dynamic library.
 #
 
 # Defined in sam.h
@@ -17,10 +18,19 @@ bam_get_qual(b::bam1_t)  = b.data + (b.core.n_cigar<<2) + b.core.l_qname + (b.co
 bam_get_aux(b::bam1_t)   = b.data + (b.core.n_cigar<<2) + b.core.l_qname + (b.core.l_qseq+1)>>1 + b.core.l_qseq
 bam_get_l_aux(b::bam1_t) = b.data - (b.core.n_cigar<<2) - b.core.l_qname - b.core.l_qseq - (b.core.l_qseq+1)>>1
 
+bam_seqi(s::Ptr{UInt8}, i::Integer) = unsafe_load(s, i>>1 + 1) >> ((~i&1)<<2) & 0xf
+
+function bam_set_mempolicy(b::Ptr{bam1_t}, policy::Integer)
+    dst = Ptr{UInt8}(b) + fieldoffset(bam1_t, 6)
+    unsafe_store!(Ptr{UInt32}(dst), convert(UInt32, policy))
+    return
+end
+bam_get_mempolicy(b::bam1_t) = b.mempolicy
+
 # Derive functions for pointer argument.
 for func in [:bam_is_rev, :bam_is_mrev, :bam_get_qname, :bam_get_cigar,
-             :bam_get_seq, :bam_get_qual, :bam_get_aux, :bam_get_l_aux]
+             :bam_get_seq, :bam_get_qual, :bam_get_aux, :bam_get_l_aux,
+             :bam_get_mempolicy]
     @eval $(func)(b::Ptr{bam1_t}) = $(func)(unsafe_load(b))
 end
 
-bam_seqi(s::Ptr{UInt8}, i::Integer) = unsafe_load(s, i>>1 + 1) >> ((~i&1)<<2) & 0xf
