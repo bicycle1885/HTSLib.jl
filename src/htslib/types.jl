5f9d241325c6f4d65a640a6e56d98c5d0dfbf814
Kenta Sato <bicycle1885@gmail.com>
2020-02-27 15:51:25 +0900

    mirror bam1_core_t and bam1_t

--- src/htslib/types.jl
+++ src/htslib/types.jl
@@ -28,7 +28,6 @@ const regidx_t = Cvoid
 const regitr_t = Cvoid
 const sam_hdr_t = Cvoid
 const samFile = htsFile
-const bam1_t = Cvoid  # TODO: mirror
 const bam_pileup1_t = Cvoid
 const bam_plp_t = Ptr{Cvoid}
 const bam_mplp_t = Ptr{Cvoid}
@@ -64,4 +63,54 @@ end
     FAI_NONE
     FAI_FASTA
     FAI_FASTQ
+end
+
+# Record flags
+"The read is paired in sequencing, no matter whether it is mapped in a pair"
+const BAM_FPAIRED        = UInt16(1 << 0)
+"The read is mapped in a proper pair"
+const BAM_FPROPER_PAIR   = UInt16(1 << 1)
+"The read itself is unmapped; conflictive with `BAM_FPROPER_PAIR`"
+const BAM_FUNMAP         = UInt16(1 << 2)
+"The mate is unmapped"
+const BAM_FMUNMAP        = UInt16(1 << 3)
+"The read is mapped to the reverse strand"
+const BAM_FREVERSE       = UInt16(1 << 4)
+"The mate is mapped to the reverse strand"
+const BAM_FMREVERSE      = UInt16(1 << 5)
+"This is read1"
+const BAM_FREAD1         = UInt16(1 << 6)
+"This is read2"
+const BAM_FREAD2         = UInt16(1 << 7)
+"Not primary alignment"
+const BAM_FSECONDARY     = UInt16(1 << 8)
+"QC failure"
+const BAM_FQCFAIL        = UInt16(1 << 9)
+"Optical or PCR duplicate"
+const BAM_FDUP           = UInt16(1 << 10)
+"Supplementary alignment"
+const BAM_FSUPPLEMENTARY = UInt16(1 << 11)
+
+struct bam1_core_t
+    pos::hts_pos_t
+    tid::Int32
+    bin::UInt16
+    qual::UInt8
+    l_extranul::UInt8
+    flag::UInt16
+    l_qname::UInt16
+    n_cigar::UInt32
+    l_qseq::Int32
+    mtid::Int32
+    mpos::hts_pos_t
+    isize::hts_pos_t
+end
+
+struct bam1_t
+    core::bam1_core_t
+    id::UInt64
+    data::Ptr{UInt8}
+    l_data::Cint
+    m_data::UInt32
+    mempolicy::UInt32
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,19 @@
 using HTSLib
+using HTSLib: htslib  # low-level API module
 using Test
 
-@test HTSLib.HTSLIB_VERSION == "1.10.2"
+# Low-level API
+# -------------
+
+@testset "Low-level API" begin
+    # check struct size (these sizes are derived from C's sizeof operator)
+    @test sizeof(htslib.bam1_core_t) == 48
+    @test sizeof(htslib.bam1_t) == 72 + sizeof(Ptr)
+end
+
+# High-level API
+# --------------
+
+@testset "High-level API" begin
+    @test HTSLib.HTSLIB_VERSION == "1.10.2"
+end
