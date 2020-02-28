f88ef45b7935dc10095b2b871a3cf308b6ca43b7
Kenta Sato <bicycle1885@gmail.com>
2020-02-28 12:17:53 +0900

    move BAM flag definitions

--- src/htslib/types.jl
+++ src/htslib/types.jl
@@ -87,32 +87,6 @@ end
 
 const samFile = htsFile  # typedef alias
 
-# Record flags
-"The read is paired in sequencing, no matter whether it is mapped in a pair"
-const BAM_FPAIRED        = UInt16(1 << 0)
-"The read is mapped in a proper pair"
-const BAM_FPROPER_PAIR   = UInt16(1 << 1)
-"The read itself is unmapped; conflictive with `BAM_FPROPER_PAIR`"
-const BAM_FUNMAP         = UInt16(1 << 2)
-"The mate is unmapped"
-const BAM_FMUNMAP        = UInt16(1 << 3)
-"The read is mapped to the reverse strand"
-const BAM_FREVERSE       = UInt16(1 << 4)
-"The mate is mapped to the reverse strand"
-const BAM_FMREVERSE      = UInt16(1 << 5)
-"This is read1"
-const BAM_FREAD1         = UInt16(1 << 6)
-"This is read2"
-const BAM_FREAD2         = UInt16(1 << 7)
-"Not primary alignment"
-const BAM_FSECONDARY     = UInt16(1 << 8)
-"QC failure"
-const BAM_FQCFAIL        = UInt16(1 << 9)
-"Optical or PCR duplicate"
-const BAM_FDUP           = UInt16(1 << 10)
-"Supplementary alignment"
-const BAM_FSUPPLEMENTARY = UInt16(1 << 11)
-
 struct bam1_core_t
     pos::hts_pos_t
     tid::Int32
